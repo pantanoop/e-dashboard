800178ac81191d0e76f586abd8da7ec30842f792
pantanoop <154772005+pantanoop@users.noreply.github.com>
2025-07-14 17:26:58 +0530

    Update inviteRoutes.js

--- server/routes/inviteRoutes.js
+++ server/routes/inviteRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { sendInvite, acceptInvite } = require("../controllers/inviteController");
 const verifyToken = require("../middleware/verifyToken");
 
-// ❗ Apply middleware ONLY to this route
+
 router.post("/invite", verifyToken, sendInvite);
 
-// ❗ DO NOT protect this route — allow public signup
+
 router.post("/accept-invite", verifyToken, acceptInvite);
 
 module.exports = router;
