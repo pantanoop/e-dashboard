b92bbc33413d85a2f9997ef5af645375033550f9
pantanoop <154772005+pantanoop@users.noreply.github.com>
2025-07-14 16:29:27 +0530

    Update OrgAccessPage.js

--- client/src/components/OrgAccessPage.js
+++ client/src/components/OrgAccessPage.js
@@ -23,7 +23,7 @@ function OrgAccessPage() {
 
     showLoading(); // ✅ spinner ON
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
