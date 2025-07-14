6acb004d325ac45df5cfbcbd7407d0e314da9270
pantanoop <154772005+pantanoop@users.noreply.github.com>
2025-07-14 22:49:13 +0530

    Delete server/routes directory

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { googleLogin, login } = require("../controllers/authController");
-
-router.post("/google-login", googleLogin);
-router.post("/login", login);
-
-module.exports = router;
