4a3fbf9e309559609779d759d9cff4b124a8ef84
pantanoop <154772005+pantanoop@users.noreply.github.com>
2025-07-15 08:14:37 +0530

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -9,7 +9,7 @@ const verifyToken = require("../middleware/verifyToken");
 const router = express.Router();
 
 router.post("/orders", verifyToken, createOrder);
-router.get("/orders/user", verifyToken, getUserOrders);
-router.get("/orders/tenant", verifyToken, getTenantOrders);
+router.get("/user", verifyToken, getUserOrders);
+router.get("/tenant", verifyToken, getTenantOrders);
 
 module.exports = router;
